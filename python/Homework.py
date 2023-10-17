9b068164756389f06532d9fd569a323e907e2f7f
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2023-10-17 10:36:19 +0200

    Delete python directory
    Moved folder into a new repository.

--- python/Homework.py
+++
@@ -1,5 +0,0 @@
-import requests
-# Originally I wanted to take some sort of a tournaments resource (either chess or from some computer games from liquipedia.com, but I found their API kind of weird and gave up) 
-req = requests.get('https://httpbin.org/get')
-
-print(req.text)
